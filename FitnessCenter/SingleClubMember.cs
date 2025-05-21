1f7c72d2de34f51907f4d017f5edaac689c3f2fd
Zach Sanford <afavorhouse2113@gmail.com>
2025-05-21 19:21:42 -0400

    added CheckIn logic

--- FitnessCenter/SingleClubMember.cs
+++ FitnessCenter/SingleClubMember.cs
@@ -8,13 +8,32 @@ namespace FitnessCenter
 {
     internal class SingleClubMember : Member
     {
-        public SingleClubMember(int memberId, string memberName, string memberPhone, string memberEmail, string memberAddress) : base(memberId, memberName, memberPhone, memberEmail, memberAddress)
+        bool checkIn = false;
+        public SingleClubMember(int memberId, string memberName, string memberPhone, string memberEmail, string memberAddress, string memberClub) : base(memberId, memberName, memberPhone, memberEmail, memberAddress, memberClub)
         {
         }
 
         public override bool CheckIn(Club club)
         {
-            return false;
+            checkIn = !checkIn;
+            try
+            {
+                club.Equals(MemberClub);
+            }catch (InvalidOperationException message)
+            {
+                Console.WriteLine(message.Message);
+                checkIn=false;
+            }
+
+            if (checkIn == true)
+            {
+                Console.WriteLine($"{MemberName} has been checked in.");
+            }
+            if  (checkIn == false)
+            {
+                Console.WriteLine($"{MemberName} has been checked out.");
+            }
+            return checkIn; 
         }
     }
 }
